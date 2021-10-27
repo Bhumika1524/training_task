b1f59f4f9d32bb9f373fce411b51a1645ad07102
Bhumika1524 <51358927+Bhumika1524@users.noreply.github.com>
2021-10-27 22:16:09 +0530

    Update task.rb

--- app/models/task.rb
+++ app/models/task.rb
@@ -5,6 +5,6 @@ class Task < ApplicationRecord
   belongs_to :from, class_name: 'User', foreign_key: 'from_id', required: false
   has_many :comments, dependent: :destroy
   accepts_nested_attributes_for :comments, reject_if: :all_blank, allow_destroy: true
-  validates_presence_of :name
+  validates_presence_of :name, :to_id, :type_id, :status_id
 
 end
